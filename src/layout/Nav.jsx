175963645118029118d2ca2e78376ebf3664d7b8
Numonu <juan.villegas.dev@gmail.com>
2023-04-17 12:06:27 -0500

    creado el menu de navegacion desktop

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -13,7 +13,7 @@ function Mobile() {
 	const [renderMenu, setRenderMenu] = useState(false);
 	return (
 		<>
-			<div className="flex justify-between items-center">
+			<div className="flex justify-between items-center xl:hidden">
 				<h1 className="text-3xl font-bold gradient-text bg-gradient-to-r from-left to-right">
 					Numonu
 				</h1>
@@ -23,7 +23,7 @@ function Mobile() {
 				></i>
 			</div>
 			{renderMenu && (
-				<ul className="space-y-2">
+				<ul className="space-y-2 text-2xl xl:hidden">
 					<Link content="inicio" href="#" />
 					<Link content="sobre mi" href="#" />
 					<Link content="servicios" href="#" />
@@ -35,13 +35,29 @@ function Mobile() {
 		</>
 	);
 }
-function Desktop() {}
+function Desktop() {
+	return (
+		<div className="hidden justify-between items-center xl:flex">
+			<h1 className="text-3xl font-bold gradient-text bg-gradient-to-r from-left to-right">
+				Numonu
+			</h1>
+			<ul className="flex gap-6 text-xl">
+				<Link content="inicio" href="#" />
+				<Link content="sobre mi" href="#" />
+				<Link content="servicios" href="#" />
+				<Link content="experiencia" href="#" />
+				<Link content="portafolio" href="#" />
+				<Link content="contacto" href="#" />
+			</ul>
+		</div>
+	);
+}
 function Link({ content, href }) {
 	return (
 		<li>
 			<a
 				href={href}
-				className="w-full inline-block text-2xl text-center uppercase hover:text-orquidea"
+				className="relative w-full inline-block text-center uppercase hover:text-orquidea transition-transform xl:hover:translate-y-1 xl:hover:scale-x-105 after:absolute xl:after:w-2 after:aspect-square after:bg-transparent after:-top-0 xl:hover:after:bg-orquidea after:rounded-full xl:after:left-1/2 xl:hover:after:-translate-y-2 xl:after:transition-all after:-translate-x-1/2"
 			>
 				{content}
 			</a>
