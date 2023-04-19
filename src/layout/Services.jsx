5390dbc233b0f172ee5cbfeec04b794057f31e0a
Numonu <juan.villegas.dev@gmail.com>
2023-04-18 19:17:19 -0500

    version desktop de la seccion 'servicios' hecha

--- src/layout/Services.jsx
+++ src/layout/Services.jsx
@@ -2,8 +2,8 @@ export function Services() {
 	return (
 		<div className="bg-dark text-light">
 			<div className="wrapper p-4 py-8">
-				<h1 className="text-3xl text-center">Servicios..</h1>
-				<div className="flex flex-col gap-12 py-6">
+				<h1 className="text-4xl text-center xl:text-5xl xl:py-6">Servicios..</h1>
+				<div className="flex flex-col flex-wrap gap-12 py-6 xl:flex-row xl:justify-center">
 					<Card
 						icon={<i className="fa-solid fa-display"></i>}
 						title="Desarrollo de Sitios Web"
@@ -26,12 +26,12 @@ export function Services() {
 }
 function Card({ icon, title, description }) {
 	return (
-		<div className="bg-dark p-4 card border-4">
-			<header className="flex items-center gap-4 mb-4">
-				<div className="bg-light text-orquidea w-20 aspect-square flex items-center justify-center rounded-full text-4xl">
+		<div className="bg-dark p-4 card border-4 sm:mx-auto sm:w-[90%] sm:px-8 xl:w-[40%] xl:mx-0">
+			<header className="flex flex-wrap justify-center items-center gap-4 mb-4 sm:justify-start">
+				<div className="bg-light text-orquidea w-16 aspect-square flex items-center justify-center shrink-0 rounded-full text-4xl">
 					{icon}
 				</div>
-				<h1 className="text-3xl uppercase">{title}</h1>
+				<h1 className="text-3xl text-center uppercase">{title}</h1>
 			</header>
 			<p className="text-zinc-300 text-2xl">{description}</p>
 		</div>
