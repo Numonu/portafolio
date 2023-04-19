e88107fe67fa07c926a00664c1ce812e92355cde
Numonu <juan.villegas.dev@gmail.com>
2023-04-18 22:37:27 -0500

    version desktop de la seccion skills hecha

--- src/layout/Experience.jsx
+++ src/layout/Experience.jsx
@@ -2,29 +2,34 @@ export function Experience() {
 	return (
 		<div className="bg-dark">
 			<div className="wrapper p-4">
-				<h1 className="text-light text-4xl text-center pb-12">
+				<h1 className="text-light text-4xl text-center pb-12 xl:text-5xl">
 					Experiencia..
 				</h1>
-				<div className="flex flex-wrap justify-center gap-12 sm:w-[60%] sm:mx-auto">
+				<div className="flex flex-wrap justify-center gap-20 sm:w-[60%] sm:mx-auto xl:w-[50%]">
 					<Chip
 						icon={<i class="fa-brands fa-html5"></i>}
 						title="html"
+                        custom="text-orange-500"
 					/>
                     <Chip
 						icon={<i class="fa-brands fa-css3-alt"></i>}
 						title="css"
+                        custom="text-blue-500"
 					/>
                     <Chip
 						icon={<i class="fa-brands fa-js"></i>}
 						title="js"
+                        custom="text-yellow-500"
 					/>
                     <Chip
 						icon={<i class="fa-brands fa-react"></i>}
 						title="react"
+                        custom="text-cyan-500"
 					/>
                     <Chip
-						icon={<i class="fa-solid fa-wind"></i>}
+						icon={<i class="fa-solid fa-ghost"></i>}
 						title="tailwind"
+                        custom="text-emerald-500"
 					/>
                     <Chip
 						icon={<i class="fa-brands fa-unity"></i>}
@@ -35,10 +40,10 @@ export function Experience() {
 		</div>
 	);
 }
-function Chip({ icon, title }) {
+function Chip({ icon, title ,custom}) {
 	return (
-		<div className="text-light flex flex-col items-center gradient-text bg-gradient-to-r from-slate-800 to-red-600">
-			<div className="text-4xl mb-2">{icon}</div>
+		<div className={`text-light flex flex-col items-center gradient-text bg-gradient-to-r from-slate-800 to-red-600 ${custom}`}>
+			<div className="text-6xl mb-2">{icon}</div>
 			<h2 className="text-2xl uppercase">{title}</h2>
 		</div>
 	);
