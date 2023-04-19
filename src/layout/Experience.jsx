ffdd0c6053b4db98c14300566d6e8836d70122fd
Numonu <juan.villegas.dev@gmail.com>
2023-04-19 09:51:46 -0500

    mas iconos en la seccion 'experiencia'

--- src/layout/Experience.jsx
+++ src/layout/Experience.jsx
@@ -7,33 +7,49 @@ export function Experience() {
 				</h1>
 				<div className="flex flex-wrap justify-center gap-20 sm:w-[60%] sm:mx-auto xl:w-[50%]">
 					<Chip
-						icon={<i class="fa-brands fa-html5"></i>}
+						icon={<i className="fa-brands fa-html5"></i>}
 						title="html"
                         custom="text-orange-500"
 					/>
                     <Chip
-						icon={<i class="fa-brands fa-css3-alt"></i>}
+						icon={<i className="fa-brands fa-css3-alt"></i>}
 						title="css"
-                        custom="text-blue-500"
+                        custom="text-cyan-500"
 					/>
                     <Chip
-						icon={<i class="fa-brands fa-js"></i>}
+						icon={<i className="fa-brands fa-js"></i>}
 						title="js"
                         custom="text-yellow-500"
 					/>
                     <Chip
-						icon={<i class="fa-brands fa-react"></i>}
+						icon={<i className="fa-brands fa-react"></i>}
 						title="react"
                         custom="text-cyan-500"
 					/>
                     <Chip
-						icon={<i class="fa-solid fa-ghost"></i>}
+						icon={<i className="fa-brands fa-git-alt"></i>}
+						title="git"
+                        custom="text-red-500"
+					/>
+					<Chip
+						icon={<i className="fa-brands fa-github"></i>}
+						title="github"
+                        custom="text-zinc-500"
+					/>
+					<Chip
+						icon={<i className="fa-brands fa-sass"></i>}
+						title="sass"
+						custom="text-pink-500"
+					/>
+					<Chip
+						icon={<i className="fa-solid fa-code"></i>}
 						title="tailwind"
-                        custom="text-emerald-500"
+						custom="text-emerald-500"
 					/>
                     <Chip
-						icon={<i class="fa-brands fa-unity"></i>}
+						icon={<i className="fa-brands fa-unity"></i>}
 						title="unity"
+						custom="text-light"
 					/>
 				</div>
 			</div>
@@ -42,9 +58,9 @@ export function Experience() {
 }
 function Chip({ icon, title ,custom}) {
 	return (
-		<div className={`text-light flex flex-col items-center gradient-text bg-gradient-to-r from-slate-800 to-red-600 ${custom}`}>
+		<div className={`flex flex-col items-center ${custom}`}>
 			<div className="text-6xl mb-2">{icon}</div>
-			<h2 className="text-2xl uppercase">{title}</h2>
+			<h2 className="text-light text-2xl uppercase">{title}</h2>
 		</div>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { About } from "./layout/About";
 import { Services } from "./layout/Services";
 import { Hero } from "./layout/Hero";
 import { Nav } from "./layout/Nav";
-import "./styles/tailwind.css";
 import { Experience } from "./layout/Experience";
+import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
