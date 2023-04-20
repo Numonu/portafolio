86bd0db3eb8a1cec26d716b4e8178e2f5dad0b4e
Numonu <juan.villegas.dev@gmail.com>
2023-04-20 10:17:02 -0500

    active el enlace del boton principal

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -11,9 +11,9 @@ export function Hero() {
 						software especializado en el desarrollo de web front-end
 					</p>
 					<button className="bg-orquidea uppercase text-2xl px-4 py-2 rounded-full glow-hover relative group overflow-hidden transition-[width]">
-						<span className="group-hover:mr-6 transition-[margin]">
+						<a href="#contact" className="group-hover:mr-6 transition-[margin]">
 							contactame
-						</span>
+						</a>
 						<i className="fa-solid fa-chevron-down text-2xl absolute -right-7 top-1/2 -translate-y-1/2 group-hover:right-2 transition-[right]"></i>
 					</button>
 				</div>
