2db04ec3bc19adb7972e15d92e76d6f29ed5effc
Numonu <juan.villegas.dev@gmail.com>
2023-05-07 17:02:31 -0500

    reveal animation en las cards de servicios

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,11 @@
-export function ServiceCard({ icon, title, description }) {
+import { motion } from "framer-motion";
+export function ServiceCard({ icon, title, description , motionDelay}) {
 	return (
-		<div className="bg-dark p-4 card border-4 sm:mx-auto sm:w-[90%] sm:px-8 xl:w-[40%] xl:mx-0">
+		<motion.div className="bg-dark p-4 card border-4 sm:mx-auto sm:w-[90%] sm:px-8 xl:w-[40%] xl:mx-0"
+			initial = {{x : -100 , opacity : 0}}
+			whileInView = {{x : 0 , opacity : 1}}
+			transition={{delay : motionDelay}}
+		>
 			<header className="flex flex-wrap justify-center items-center gap-4 mb-4 sm:justify-start">
 				<div className="bg-light text-orquidea w-16 aspect-square flex items-center justify-center shrink-0 rounded-full text-4xl">
 					{icon}
@@ -8,6 +13,6 @@ export function ServiceCard({ icon, title, description }) {
 				<h1 className="text-2xl text-center uppercase">{title}</h1>
 			</header>
 			<p className="text-zinc-300 text-2xl">{description}</p>
-		</div>
+		</motion.div>
 	);
 }
--- src/layout/Services.jsx
+++ src/layout/Services.jsx
@@ -15,11 +15,13 @@ export function Services() {
 						icon={<i className="fa-solid fa-cube"></i>}
 						title="Diseño y Modelado 3D"
 						description="Ofrezco servicios de modelado y diseño 3D para ayudar a que su proyecto cobre vida. Con experiencia en la creación de modelos 3D animados y coloridos para una variedad de industrias, puedo ayudarle a visualizar su producto."
+						motionDelay={.2}
 					/>
 					<ServiceCard
 						icon={<i className="fa-solid fa-gamepad"></i>}
 						title="Desarrollo de Videojuegos"
 						description="Ofrezco servicios de desarrollo de videojuegos para convertir tus ideas en experiencias interactivas emocionantes y atractivas. Desde la conceptualización hasta la publicación, puedo ayudarte a crear juegos personalizados que cautiven a tu audiencia"
+						motionDelay={.4}
 					/>
 				</div>
 			</div>
