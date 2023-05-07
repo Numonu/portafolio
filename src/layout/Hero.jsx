f514b9d2d0493b4c5dc6859b0ff099a13312bc8c
Numonu <juan.villegas.dev@gmail.com>
2023-05-07 16:24:09 -0500

    agrupe las animaciones de los componentes  en una sola variante

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -1,4 +1,10 @@
 import { motion } from "framer-motion";
+
+const motionReveal = {
+	hidden : {opacity: 0, y: 150},
+	show : {opacity: 1, y: 0},
+}
+
 export function Hero() {
 	return (
 		<div className="px-4 pt-6 bg-[url('../images/wave.svg')] bg-no-repeat bg-bottom">
@@ -6,26 +12,29 @@ export function Hero() {
 				<div className="flex flex-col items-center gap-8 text-center xl:w-1/2 xl:justify-center xl:items-start xl:text-start">
 					<motion.h1
 						className="text-orquidea text-4xl font-bold xl:text-5xl"
-						initial={{ opacity: 0, y: 150 }}
-						animate={{ opacity: 1, y: 0 }}
+						initial="hidden"
+						animate="show"
 						transition={{ delay: 1 }}
+						variants={motionReveal}
 					>
 						Front-End Web Developer
 					</motion.h1>
 					<motion.p
 						className="text-light text-2xl sm:w-[70%] xl:w-full"
-						initial={{ opacity: 0, y: 150 }}
-						animate={{ opacity: 1, y: 0 }}
+						initial="hidden"
+						animate="show"
 						transition={{ delay: 1.2 }}
+						variants={motionReveal}
 					>
 						Hola! , me llamo Juan David soy desarrollador de
 						software especializado en desarrollo web front-end
 					</motion.p>
 					<motion.button
 						className="bg-orquidea uppercase text-2xl px-4 py-2 rounded-full glow-hover relative group overflow-hidden transition-[width]"
-						initial={{ opacity: 0, y: 150 }}
-						animate={{ opacity: 1, y: 0 }}
+						initial="hidden"
+						animate="show"
 						transition={{ delay: 1.4 }}
+						variants={motionReveal}
 					>
 						<a
 							href="#contact"
@@ -38,8 +47,8 @@ export function Hero() {
 				</div>
 				<motion.div
 					className="h-72 bg-[url('../images/avatar.webp')] bg-contain bg-bottom bg-no-repeat xl:w-1/2 xl:h-full"
-					initial={{ transformOrigin: "bottom" }}
-					animate={{ scale: [0 , 1.1 , 1] }}
+					initial={{ scale : 0 , transformOrigin: "bottom" }}
+					animate={{ scale: 1}}
 					transition={{ delay: 1.6 }}
 				></motion.div>
 			</div>
