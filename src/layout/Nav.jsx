46c589fc23ba9773a1d2849273f20f47e45171eb
Numonu <juan.villegas.dev@gmail.com>
2023-05-07 16:18:08 -0500

    animacion del logo en mobile y desktop

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Link } from "../components/Link";
 import { useState } from "react";
 
@@ -15,9 +16,14 @@ function Mobile() {
 	return (
 		<>
 			<div className="flex justify-between items-center xl:hidden">
-				<h1 className="text-3xl font-bold gradient-text bg-gradient-to-r from-left to-right">
+				<motion.h1
+					className="text-3xl font-bold gradient-text bg-gradient-to-r from-left to-right"
+					initial={{ scaleX: 0 }}
+					animate={{ scaleX: 1 }}
+					transition={{ delay: 0.5 }}
+				>
 					Numonu
-				</h1>
+				</motion.h1>
 				<i
 					className="fa-solid fa-bars text-2xl hover:text-orquidea"
 					onClick={() => setRenderMenu(!renderMenu)}
@@ -39,16 +45,21 @@ function Mobile() {
 function Desktop() {
 	return (
 		<div className="hidden justify-between items-center xl:flex">
-			<h1 className="text-3xl font-bold gradient-text bg-gradient-to-r from-left to-right">
+			<motion.h1
+				className="text-3xl font-bold gradient-text bg-gradient-to-r from-left to-right"
+				initial={{ scaleX: 0 }}
+				animate={{ scaleX: 1 }}
+				transition={{ delay: 0.5 }}
+			>
 				Numonu
-			</h1>
+			</motion.h1>
 			<ul className="flex gap-6 text-2xl">
-				<Link content="inicio" href="#" delay={.5}/>
-				<Link content="sobre mi" href="#about" delay={.6}/>
-				<Link content="servicios" href="#services" delay={.7}/>
-				<Link content="experiencia" href="#experience" delay={.8}/>
-				<Link content="portafolio" href="#portfolio" delay={.9}/>
-				<Link content="contacto" href="#contact" delay={1}/>
+				<Link content="inicio" href="#" delay={0.5} />
+				<Link content="sobre mi" href="#about" delay={0.6} />
+				<Link content="servicios" href="#services" delay={0.7} />
+				<Link content="experiencia" href="#experience" delay={0.8} />
+				<Link content="portafolio" href="#portfolio" delay={0.9} />
+				<Link content="contacto" href="#contact" delay={1} />
 			</ul>
 		</div>
 	);
