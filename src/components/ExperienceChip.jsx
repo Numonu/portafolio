124005068378822fb7428f782d85959b2f6de248
Numonu <juan.villegas.dev@gmail.com>
2023-05-07 17:12:37 -0500

    reveal animation en los chips de experiencia

--- src/components/ExperienceChip.jsx
+++ src/components/ExperienceChip.jsx
@@ -1,6 +1,12 @@
-export function ExperienceChip({ icon, title ,custom}) {
+import { motion } from "framer-motion";
+export function ExperienceChip({ icon, title, custom }) {
 	return (
-		<div className={`flex flex-col items-center ${custom}`}>
+		<div className={`relative flex flex-col items-center ${custom}`}>
+			<motion.div
+				className="bg-dark absolute inset-0"
+				initial={{ scaleX: 1 , transformOrigin : "left center" }}
+				whileInView={{scaleX : 0}}
+			></motion.div>
 			<div className="text-6xl mb-2">{icon}</div>
 			<h2 className="text-light text-2xl uppercase">{title}</h2>
 		</div>
